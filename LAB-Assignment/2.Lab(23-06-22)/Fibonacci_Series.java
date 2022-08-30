013b0f535add1b8b0c8d39f456893b5666f54e35
Ritik Nipane <100013538+RitzyKingS@users.noreply.github.com>
2022-08-30 11:55:18 +0530

    Update Fibonacci_Series.java

--- LAB-Assignment/2.Lab(23-06-22)/Fibonacci_Series.java
+++ LAB-Assignment/2.Lab(23-06-22)/Fibonacci_Series.java
@@ -9,18 +9,16 @@ class Fibonacci_Series
         Scanner sc = new Scanner(System.in);
         int n1, n2,n3,count;
         
-        // asking the starting value and last value
         System.out.print("Please enter the two value for Fibonacci Series : ");
         n1 = sc.nextInt();
         n2 = sc.nextInt();
         System.out.println(" Your starting value is : "+n1+"   Your Ending value is : "+n2);
         
-        // asking the no of count that loop will be run
         System.out.println("Please enter the Counting value : ");
         count = sc.nextInt();
         for(int i=2 ; i<count; ++i)
         {
-            // adding the stating value and last value
+          
             n3 = n1+n2;
             System.out.print(" "+n3);
             // interchange value
@@ -28,11 +26,9 @@ class Fibonacci_Series
             n2 = n3;
         }
     }
-    
-    // main method
+   
     public static void main(String[] args)
     {
-        // fibo method calling
         fibo();
     }
 }
